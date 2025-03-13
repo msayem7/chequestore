c2dc526d57430accee7035131d39c1a99790b7af
“msayem7” <msayem7@gmail.com>
2025-03-13 19:34:23 +0600

    Clean up gitignore and remove cached files

--- chequestore/wsgi.py
+++ chequestore/wsgi.py
@@ -1,13 +1,10 @@
-"""
-WSGI config for chequestore project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add the project directory to the Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
