dceaab4b04ccd0745c2aeeca241c4bc1cdd82c51
“msayem7” <msayem7@gmail.com>
2025-04-14 00:42:24 +0600

    Fine Tune

--- cheques/views.py
+++ cheques/views.py
@@ -142,19 +142,6 @@ class HasCustomerActivity(viewsets.ModelViewSet):
         except Customer.DoesNotExist:
             return False
         
-# class HasCustomerActivity(viewsets.ModelViewSet):
-#     queryset = Customer.objects.all()
-#     serializer_class = serializers.CustomerSerializer
-
-#     def has_Activity(self, request, *args, **kwargs):
-#         try:
-#             customer = get_object_or_404(Customer, alias_id=request.parser_context['kwargs']['alias_id'])
-#             has_activity = customer.creditinvoice_set.exists() or customer.chequestore_set.exists()
-#             return has_activity
-#         except Customer.DoesNotExist:
-#             return False
-
-
 class CreditInvoiceViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CreditInvoiceSerializer
     queryset = CreditInvoice.objects.all()
