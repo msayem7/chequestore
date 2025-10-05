468a8021b2434561c712834c08fc5cafdef031ea
“msayem7” <msayem7@gmail.com>
2025-10-05 11:15:21 +0600

    update settings for deployment

--- src/settings.py
+++ src/settings.py
@@ -211,27 +211,29 @@ WSGI_APPLICATION = 'src.wsgi.application'
 #     }
 # }
 
-# production Database settings for PostgreSQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DJANGO_DB_NAME', os.environ.get('POSTGRES_DB', 'chequestoredb')),
-#         'USER': os.environ.get('DJANGO_DB_USER', os.environ.get('POSTGRES_USER', 'chequestoreuser')),
-#         'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', os.environ.get('POSTGRES_PASSWORD', 'adam01')),
-#         'HOST': os.environ.get('DJANGO_DB_HOST', os.environ.get('DB_HOST', 'localhost')),
-#         'PORT': os.environ.get('DJANGO_DB_PORT', os.environ.get('DB_PORT', '5432')),
-#     }
-# }
+# production Database settings for PostgreSQL in docker container
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'chequestore'),      # Database name: ezchq
-        'USER': os.environ.get('DB_USER', 'chequestoreuser'),      # Database user : postgres
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'adam01'),  # Database password: chequestorepass
-        'HOST': os.environ.get('DB_HOST', 'localhost'),         # Database host (e.g., 'localhost' or IP)
-        'PORT': os.environ.get('DB_PORT', '5432'),              # Default PostgreSQL port
+        'NAME': os.environ.get('DJANGO_DB_NAME', os.environ.get('POSTGRES_DB', 'chequestoredb')),
+        'USER': os.environ.get('DJANGO_DB_USER', os.environ.get('POSTGRES_USER', 'chequestoreuser')),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', os.environ.get('POSTGRES_PASSWORD', 'adam01')),
+        'HOST': os.environ.get('DJANGO_DB_HOST', os.environ.get('DB_HOST', 'localhost')),
+        'PORT': os.environ.get('DJANGO_DB_PORT', os.environ.get('DB_PORT', '5432')),
     }
 }
+
+#Development Database settings for PostgreSQL localhost
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB', 'chqbd'),      # Database name: ezchq
+#         'USER': os.environ.get('POSTGRES_USER', 'postgres'),      # Database user : postgres
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'sa1234'),  # Database password: chequestorepass
+#         'HOST': os.environ.get('DB_HOST', 'localhost'),         # Database host (e.g., 'localhost' or IP)
+#         'PORT': os.environ.get('DB_PORT', '5432'),              # Default PostgreSQL port
+#     }
+# }
 
 # # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 # DATABASES = {
